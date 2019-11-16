f7e05745b56bf50890978d4e3a4889660fb328f3
Prince Oppong Boamah <peedor@Princes-MacBook-Pro.local>
2019-11-16 06:49:19 +0000

    Prince: edited this script to delete a property

--- js_Basics/pure_js/dog.js
+++ js_Basics/pure_js/dog.js
@@ -5,3 +5,13 @@ var myDog = {
   tails: 1,
   friends: ["Everyone"]
 };
+
+//updating object property with a new vaalue .
+myDog.name = "Rejoice";
+
+//adding a new property to the dog variable
+
+myDog.bark = "Woof-woof!";
+
+//delete a property from the dog variable
+//delete myDog.friends;
