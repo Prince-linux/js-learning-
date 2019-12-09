179dfea150bf895cca224f0efbc94eabcfc2e8f5
Prince Oppong Boamah <peedor@Princes-MacBook-Pro.local>
2019-12-09 12:50:23 +0000

    Added one more code

---
+++ js_Basics/pure_js/freeze.js
@@ -0,0 +1,20 @@
+// A simple implementation of const and freeze to not change the value of pi
+
+function freezeObj() {
+    "use strict";
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+
+    Object.freeze(MATH_CONSTANTS);
+
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch (ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+console.log(PI)
